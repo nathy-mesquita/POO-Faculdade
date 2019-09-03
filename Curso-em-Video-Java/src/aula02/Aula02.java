16d03fe4798cac9f7ad6ebcb658734821f62f5bf
Nathaly Mesquita <nnathalygm@gmail.com>
2019-09-03 00:14:20 -0300

    Adicionando uma nova brach - curso em video

---
+++ Curso-em-Video-Java/src/aula02/Aula02.java
@@ -0,0 +1,15 @@
+package aula02;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+	//Instanciar um objeto "c1" do tipo Caneta
+	 Caneta c1 = new Caneta();
+	 c1.cor = "Azul";
+	 c1.ponta = 0.5f;
+	 c1.tampada = false;
+	 c1.status();
+
+	}
+
+}
